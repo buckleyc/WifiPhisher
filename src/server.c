562e8e64a0fd9b1b2aff6cb7097e08ff77ff8049
Alessandro D'Alterio <alessandro.dalterio@teoresigroup.com>
2024-11-16 01:31:57 +0100

    Edited sdk configuration and monitor speed

--- src/server.c
+++ src/server.c
@@ -4,10 +4,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "spi_flash_mmap.h"
 
 #include "esp_wifi.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "freertos/event_groups.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
